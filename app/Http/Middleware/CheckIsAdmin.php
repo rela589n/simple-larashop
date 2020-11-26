8c757803dc15c853d2c7b085e789362f260bbb83
rela589n <zsidelnik@gmail.com>
2020-11-26 08:16:58 +0200

    fixup(middleware): tweak message in CheckIsAdmin

--- app/Http/Middleware/CheckIsAdmin.php
+++ app/Http/Middleware/CheckIsAdmin.php
@@ -19,7 +19,7 @@ class CheckIsAdmin
         $user = Auth::user();
 
         if (!$user->isAdmin()) {
-            session()->flash('warning', 'У вас нет прав администратора');
+            session()->flash('warning', 'Ви повинні бути адміністратором, щоб переглядати цю сторінку');
             return redirect()->route('index');
         }
 
